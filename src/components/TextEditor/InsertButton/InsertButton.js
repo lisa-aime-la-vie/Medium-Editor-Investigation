4b934ec197d3f562980c637e7319deba1a3dc8ed
lwang9 <lisa.wang@activenetwork.com>
2018-10-30 11:18:00 +0800

    feat: add customized button

--- src/components/TextEditor/InsertButton/InsertButton.js
+++ src/components/TextEditor/InsertButton/InsertButton.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
+import extend from 'lodash/extend'
+import MediumEditor from 'medium-editor'
 
 class InsertButton extends React.Component<*, *> {
   constructor() {
@@ -8,10 +10,88 @@ class InsertButton extends React.Component<*, *> {
       isShow: false
     }
   }
-  handleClick = () => {
+  handleToggle = () => {
     this.setState(prev => ({
       isShow: !prev.isShow
     }))
+  }
+
+  handleItemSelected = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.insertText(['{', e.target.getAttribute('value'), '}'].join(''));
+    this.handleToggle();
+  }
+
+  insertText = text => {
+    const range = this.getSelectionRange()
+    let newNode
+    let dynamicNode
+    if (range) {
+      if (range.startContainer.nodeName === 'DIV' && range.startContainer.className === 'editor') {
+        dynamicNode = this.createDynamicContentNode(text)
+        newNode = document.createElement('p').append(dynamicNode)
+      } else {
+        newNode = this.createDynamicContentNode(text)
+        dynamicNode = newNode
+      }
+
+      const dynamicContent = this.getDynamicContent(range.startContainer)
+      if (range.collapsed && this.isDynamicContent(dynamicContent) &&
+        (range.startOffset === 0 || range.startOffset === range.startContainer.length)) {
+        if (range.startOffset === 0) {
+          dynamicContent.parentNode.insertBefore(newNode, dynamicContent)
+        } else {
+          dynamicContent.parentNode.insertBefore(newNode, dynamicContent.nextSibling)
+        }
+      } else {
+        this.deleteRangeContent(range);
+        range.insertNode(newNode);
+      }
+      MediumEditor.selection.moveCursor(document, dynamicNode.lastChild, dynamicNode.lastChild.textContent.length,
+        dynamicNode.lastChild, dynamicNode.lastChild.textContent.length);
+    }
+  }
+
+  deleteRangeContent = range => {
+    [range.startContainer, range.endContainer].forEach( element => {
+      const dynamicContent = this.getDynamicContent(element)
+      if (this.isDynamicContent(dynamicContent)) {
+        dynamicContent.parentNode.removeChild(dynamicContent)
+      }
+    }, this);
+    range.deleteContents();
+  }
+
+  isDynamicContent = content => {
+    return content === null ? false : true;
+  }
+
+  getDynamicContent = rangeContainer => {
+    let ele = extend({}, rangeContainer.parentNode)
+    while(ele && ele.className.indexOf('dynamic-content') < 0) {
+      if (ele && ele.className.indexOf('editor') < 0) {
+        ele = ele.parentNode
+      } else {
+        ele = null
+      }
+    }
+    return ele
+  }
+
+  createDynamicContentNode = text => {
+    const span = document.createElement('span')
+    span.className = 'dynamic-content'
+    span.append(text)
+    return span
+}
+
+  getSelectionRange = () => {
+    const selection = window.getSelection()
+    if (selection && selection.rangeCount) {
+      return selection.getRangeAt(0)
+    }
+    return null
   }
 
   render() {
@@ -30,14 +110,14 @@ class InsertButton extends React.Component<*, *> {
     const { isShow } = this.state
     return (
       <div className="dropdown">
-        <button className="dropdown-toggle" id="dropdownMenuButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.handleClick}>
+        <button className="dropdown-toggle" id="dropdownMenuButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.handleToggle}>
           Insert object
         </button>
-        <div className={classNames("dropdown-menu", { "show" : isShow })} aria-labelledby="dropdownMenuButton">
+        <div className={classNames("dropdown-menu", "dynamic-content", { "show" : isShow })} aria-labelledby="dropdownMenuButton">
         {
           dynamicContent.map(ele => {
             return (
-              <a className="dropdown-item" tabindex="-1" href="#">{ele.labelKey}</a>
+              <a className="dropdown-item" tabIndex="-1" href="#" onClick={this.handleItemSelected} value={ele.value}>{ele.labelKey}</a>
             )
           })
         }
